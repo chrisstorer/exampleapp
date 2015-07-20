3b4dc52cfdb8b9e4226c76727ccd4032ad73ac5b
chris <chris@chrisstorer.co.uk>
2015-07-20 12:09:06 +0100

    Initial working version

---
+++ src/main/java/exampleapp/configuration/ThymeLeafConfiguration.java
@@ -0,0 +1,30 @@
+package exampleapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+/**
+ * Created by Chris on 20/07/2015.
+ */
+@Configuration
+public class ThymeLeafConfiguration {
+
+    /*
+    @Bean
+    public ViewResolver viewResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setTemplateMode("XHTML");
+        templateResolver.setPrefix("views/");
+        templateResolver.setSuffix(".html");
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver);
+
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(engine);
+        return viewResolver;
+    }*/
+}
---
+++ src/main/java/exampleapp/controllers/HelloWorldController.java
@@ -0,0 +1,22 @@
+package exampleapp.controllers;
+
+import org.springframework.core.SpringVersion;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Chris on 20/07/2015.
+ */
+@Controller
+public class HelloWorldController
+{
+
+    @RequestMapping("/helloworld")
+    public String helloworld(HttpServletRequest request,HttpServletResponse response)
+    {
+        return "helloworld";
+    }
+}
